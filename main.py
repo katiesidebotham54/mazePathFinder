2b5eba1de3a71debfe6f6f6bd17dc2f6e15bdd52
Katie Sidebotham <katiesidebotham54@gmail.com>
2023-02-06 17:22:51 -0500

    maze generator aka 2d binary matrix

--- main.py
+++ main.py
@@ -1,1 +1,73 @@
-print("Hello World")
+import random
+
+
+def generate_maze(n):
+    maze = [[0 for j in range(n)] for i in range(n)]
+    visited = [[False for j in range(n)] for i in range(n)]
+    stack = []
+
+    start_i, start_j = random.randint(0, n - 1), random.randint(0, n - 1)
+    visited[start_i][start_j] = True
+    stack.append((start_i, start_j))
+
+    while stack:
+        i, j = stack[-1]
+        unvisited_neighbours = []
+        if i > 0 and not visited[i - 1][j]:
+            unvisited_neighbours.append((i - 1, j))
+        if i < n - 1 and not visited[i + 1][j]:
+            unvisited_neighbours.append((i + 1, j))
+        if j > 0 and not visited[i][j - 1]:
+            unvisited_neighbours.append((i, j - 1))
+        if j < n - 1 and not visited[i][j + 1]:
+            unvisited_neighbours.append((i, j + 1))
+
+        if unvisited_neighbours:
+            next_i, next_j = random.choice(unvisited_neighbours)
+            if random.random() < 0.7:
+                visited[next_i][next_j] = True
+                stack.append((next_i, next_j))
+            else:
+                maze[next_i][next_j] = 1
+        else:
+            stack.pop()
+
+    for i in range(n):
+        for j in range(n):
+            if not visited[i][j]:
+                start_i, start_j = i, j
+                break
+
+    stack = []
+    visited = [[False for j in range(n)] for i in range(n)]
+    visited[start_i][start_j] = True
+    stack.append((start_i, start_j))
+
+    while stack:
+        i, j = stack[-1]
+        unvisited_neighbours = []
+        if i > 0 and not visited[i - 1][j]:
+            unvisited_neighbours.append((i - 1, j))
+        if i < n - 1 and not visited[i + 1][j]:
+            unvisited_neighbours.append((i + 1, j))
+        if j > 0 and not visited[i][j - 1]:
+            unvisited_neighbours.append((i, j - 1))
+        if j < n - 1 and not visited[i][j + 1]:
+            unvisited_neighbours.append((i, j + 1))
+
+        if unvisited_neighbours:
+            next_i, next_j = random.choice(unvisited_neighbours)
+            visited[next_i][next_j] = True
+            stack.append((next_i, next_j))
+        else:
+            stack.pop()
+
+    return maze
+
+
+n = 101
+maze = generate_maze(n)
+sourceFile = open('maze.txt', 'w')
+for row in maze:
+    print(row, file=sourceFile)
+sourceFile.close()

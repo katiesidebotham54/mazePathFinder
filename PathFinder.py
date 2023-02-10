af33915571ad50fcf39c61cbc861550646213cf4
Jasper Van Der Ree <66978426+Jasper-Van-Der-Ree@users.noreply.github.com>
2023-02-10 00:02:45 -0500

    Update PathFinder.py
    fml

--- PathFinder.py
+++ PathFinder.py
@@ -2,19 +2,28 @@ def man_dist(n1, n2):
 
 	return n1[0] - n2[0] + n1[1] - n2[1] # (x1 - x2) + (y1 - y2)
 
-def f(state, agent, goal):
+def compute_path(maze): #1
 
-	return man_dist(state, agent) + man_dist(state, goal) # f(s) = g(s) + h(s)
+	return
 
-def repeated_forward_a_star(mazes):
+def repeated_forward(mazes):
 
 	while mazes:
 
 		maze = mazes[0]
 
+		while maze: #14
 
+			counter = 0 #15
+			open_list = []
+			closed_list = []
+			search = np.zeros((len(maze),len(maze[0]))) #16 & 17
 
+			while True:
 
+				counter += 1
+
+			compute_path(maze)
 
 		mazes.pop(0)
 

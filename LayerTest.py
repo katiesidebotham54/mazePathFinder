3082862aca7740e90c526843f55c2f78a43cc124
Jasper Van Der Ree <66978426+Jasper-Van-Der-Ree@users.noreply.github.com>
2023-02-15 00:35:05 -0500

    VICTORY

--- LayerTest.py
+++ LayerTest.py
@@ -1,33 +1,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import main
-
-n = 101
+import time
 
 class state():
     def __init__(self, parent=None, position=None):
         self.parent = parent
         self.position = position
 
-pathdict = {"state0": state(None, (0,0))}
+#Array Size
+n = 101
 
+#PathDict - Dictionary containing test states
+pathdict = {"state0": state(None, (0,0))}
 for x in range(1, n, 1):
 	key = "state" + str(x)
 	pathdict[key] = state(pathdict["state" + str(x-1)], (x,x))
 
-path = np.zeros((n,n), dtype = int)
-for x in pathdict.values():
-	path[x.position[0]][x.position[1]] = 1
-
-#MatPlotLib Experimentation
+#Figure
+fig,ax = plt.subplots()
 
 #Maze
-plt.imshow(main.generate_maze(n), alpha = .75, cmap = 'binary')
-
-#Masks all 0 values in the path to make only the actual path visible
-path = np.ma.masked_where(path == 0, path)
+maze = main.generate_maze(n)
 
 #Path
-plt.imshow(path, alpha = 1, cmap = 'autumn')
+path = np.zeros((n,n), dtype = int)
+path = np.ma.masked_where(path == 0, path)
 
+#Animated Path
+im = plt.imshow(path, alpha = 1, cmap = 'autumn',\
+				interpolation='none', aspect='auto', vmin=0, vmax=1)
+
+#Update the figure with one additional node
+def animate(i):
+	if i == len(pathdict):
+		print( '.', end = '')
+	else:
+		path[list(pathdict.values())[i].position[0]]\
+			[list(pathdict.values())[i].position[1]] = 1
+		ax.clear()
+		plt.imshow(maze, alpha = .5, cmap = 'binary')
+		im = plt.imshow(path, alpha = 1, cmap = 'autumn', animated = True)
+
+ani = animation.FuncAnimation(fig, animate, frames = len(pathdict), interval = 50)
+plt.imshow(maze, alpha = .75, cmap = 'binary')
 plt.show()
+
+
+#End Result
+# plt.imshow(maze, alpha = .75, cmap = 'binary')
+# plt.imshow(path, alpha = 1, cmap = 'autumn')
+# plt.show()

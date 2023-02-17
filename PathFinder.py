30a74953aa6d591c99c22086f94641271113869e
Jasper Van Der Ree <66978426+Jasper-Van-Der-Ree@users.noreply.github.com>
2023-02-16 21:24:12 -0500

    Updates to visualizing path

--- PathFinder.py
+++ PathFinder.py
@@ -19,7 +19,7 @@ CLOSED_LIST = set()
 actions = ["up", "down", "left", "right"]
 n = 11
 GRID = main.generate_maze(n)
-pathlist = []
+Fake_Closed_List = []
 counter = 0
 
 
@@ -40,32 +40,70 @@ class state():
 
 class animated_path():
 
-    def __init__(self, maze, pathlist):
+    def __init__(self, maze, clv_list, pv_list):
+
+        #Layer 1: Maze (AKA GRID) Visualization
         self.maze = maze
-        self.path = np.zeros(maze.shape, dtype = int)
-        self.path = np.ma.masked_where(self.path == 0, self.path)
-        self.term = np.zeros(maze.shape, dtype = int)
-        self.term = np.ma.masked_where(self.term == 0, self.term)
-        self.pathlist = pathlist
+
+        #Layer 2: Closed List Visualization
+        self.clv = np.zeros(maze.shape, dtype = int)
+        self.clv = np.ma.masked_where(self.clv == 0, self.clv)
+
+        #Layer 3: Path Visualization
+        self.pv = np.zeros(maze.shape, dtype = int)
+        self.pv = np.ma.masked_where(self.pv == 0, self.pv)
+
+        #Layer 4: Goal State Visualization
+        self.gsv = np.zeros(maze.shape, dtype = int)
+        self.gsv = np.ma.masked_where(self.gsv == 0, self.gsv)
+
+        #Closed List
+        self.clv_list = clv_list
+
+        #Path List
+        self.pv_list = pv_list
+
+        #Figure & Axis Init
         self.fig, self.ax = plt.subplots()
+
+    def update_pv(self, i):
+
+        i -= len(self.clv_list)
+
+        self.pv[self.pv_list[i].position[0]][self.pv_list[i].position[1]] = 1
+
+    def update_clv(self, i):
+
+        self.clv[self.clv_list[i].position[0]][self.clv_list[i].position[1]] = 1
+
+    def update_gsv(self, i):
+
+        self.gsv[self.clv_list[i].position[0]][self.clv_list[i].position[1]] = 1
 
     def animate(self, i):
 
-        if i >= len(self.pathlist):
+        if i >= len(self.clv_list) + len(self.pv_list):
             print('.', end = '')
             return
 
-        elif i == (len(self.pathlist) - 1):
-            self.term[self.pathlist[i].position[0]][self.pathlist[i].position[1]] = 1
+        elif i >= len(self.clv_list):
+            self.update_path(i)
 
+        elif i == (len(self.clv_list) - 1):
+            self.update_gsv(i)
+
+        else:
+            self.update_clv(i)
+
+        #Show All Layers
         self.ax.clear() 
-        self.path[self.pathlist[i].position[0]][self.pathlist[i].position[1]] = 1
         plt.imshow(self.maze, alpha = 1, cmap = 'binary')
-        plt.imshow(self.path, alpha = 1, cmap = 'cool')
-        plt.imshow(self.term, alpha = 1, cmap = 'spring')
+        plt.imshow(self.clv, alpha = 1, cmap = 'cool')
+        plt.imshow(self.pv, alpha = 1, cmap = 'summer')
+        plt.imshow(self.gsv, alpha = 1, cmap = 'spring')
 
     def start_animation(self):
-        anim = animation.FuncAnimation(self.fig, self.animate, frames = len(self.path), interval = 1)
+        anim = animation.FuncAnimation(self.fig, self.animate, frames = len(self.clv_list), interval = 50)
         plt.show()
 
 def a_star(start_s, goal_s):
@@ -79,13 +117,12 @@ def a_star(start_s, goal_s):
         curr_f, curr_s = heappop(OPEN_LIST)
         print("iteration: " + str(counter))
         print(f"s_curr: {curr_s.position}")
-        pathlist.append(curr_s)
+        Fake_Closed_List.append(curr_s)
         if curr_s.equals(goal_s):
             path = []
-            s = goal_s
+            s = curr_s
             while s is not None:
                 path.append(s)
-                # print(s.parent)
                 s = s.parent
             path.reverse()  
             return path, curr_s.g
@@ -94,10 +131,11 @@ def a_star(start_s, goal_s):
         # for each neighbor of current node
         for a in actions:
             succ_s = succ(curr_s, a)
-            print(f"succ_s: {succ_s}")
             if succ_s is None:
+                print(f"succ_s: {succ_s}")
                 continue
             else:
+                print(f"succ_s: {succ_s.position}")
                 new_g = curr_s.g + 1
                 if succ_s.position in [s.position for s in CLOSED_LIST] and new_g >= succ_s.g:
                     continue
@@ -155,15 +193,19 @@ def main():
     OPEN_LIST.clear()
     CLOSED_LIST.clear()
     path, min_cost = a_star(start_s, goal_s)
-    print([s.position for s in path])
+    if path:
+        print([s.position for s in path])
+    else:
+        print(path)
     print("min cost: " + str(min_cost))
 
-    # Visualize Traversal
-    exploration = animated_path(GRID, pathlist)
+    # Visualize Closed List
+    exploration = animated_path(GRID, Fake_Closed_List, path)
     exploration.start_animation()
 
-    path_vis = animated_path(GRID, path)
-    path_vis.start_animation()
+    # Visualize Path
+    # path_vis = animated_path(GRID, path)
+    # path_vis.start_animation()
 
 
 if __name__ == "__main__":
